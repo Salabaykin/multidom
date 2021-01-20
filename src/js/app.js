50af62640ab2eae672951210770eee1497b0a576
Evgeny <salabaikin@mail.ru>
2021-01-20 18:44:38 +0300

    Finish

---
+++ src/js/app.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  $('.category-slider').slick({
+    slidesToShow: 4,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 510,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ],
+  });
+
+  var slider = $('.footer-slider').slick({
+    slidesToShow: 4,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 510,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ],
+  });
+
+  const showSlide = document.querySelector('.footer-slider__show'),
+        hideSlide = document.querySelector('.slider-hide'),
+        sliderItem = document.querySelectorAll('.footer-slider__block');
+
+  showSlide.addEventListener('click', () => {
+    sliderItem.forEach(item => {
+      showSlide.style = 'display:none';
+      hideSlide.style = 'display:block';
+      item.style = 'height:100%';
+    });
+    slider.slick('refresh');
+  });
+
+  hideSlide.addEventListener('click', () => {
+    sliderItem.forEach(item => {
+      showSlide.style = 'display:block';
+      hideSlide.style = 'display:none';
+      item.style = 'height:150px';
+    });
+    slider.slick('refresh');
+  });
+
+});
+
